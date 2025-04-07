aee8cafd33fc3bb86827aff3f77c7b5e65a32ea9
YiipHuhu <alan.gabriel@nullbug.dev>
2025-04-06 22:04:39 -0300

    ajuste de tratamento de msg na confirmação de presença

--- src/app/participants/[event_id]/page.tsx
+++ src/app/participants/[event_id]/page.tsx
@@ -117,56 +117,61 @@ const EventDetails: React.FC = () => {
     return (    
         <>
             <div className={EventParticipants.card}>
-                <EventCard 
-                    name={eventAttributes.name}
-                    eventStart={eventAttributes.event_start}
-                    eventEnd={eventAttributes.event_end}
-                    course={courseAttributes?.name || ''}
-                    location={`${eventAttributes.location.amenity}, ${eventAttributes.location.road}, ${eventAttributes.location.town}, ${eventAttributes.location.state}, ${eventAttributes.location.postcode}`}
-                    description={eventAttributes.description}
-                />
+            <EventCard 
+                name={eventAttributes.name}
+                eventStart={eventAttributes.event_start}
+                eventEnd={eventAttributes.event_end}
+                course={courseAttributes?.name || ''}
+                location={`${eventAttributes.location.amenity}, ${eventAttributes.location.road}, ${eventAttributes.location.town}, ${eventAttributes.location.state}, ${eventAttributes.location.postcode}`}
+                description={eventAttributes.description}
+            />
             </div>
             <div className={`${EventParticipants.confirmButtonContainer} ${urbanist.className}`}>
-                <button 
-                    className={`${EventParticipants.confirmButton} ${urbanist.className}`} 
-                    onClick={handleLocationAndOpenModal}
-                    disabled={!coords}
-                >
-                    Confirmar Presença
-                </button>
+            <button 
+                className={`${EventParticipants.confirmButton} ${urbanist.className}`} 
+                onClick={handleLocationAndOpenModal}
+                disabled={!coords}
+            >
+                Confirmar Presença
+            </button>
             </div>
 
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                <div className={EventParticipants.modalContent}>
-                    {presenceConfirmed ? (
-                        <p className={EventParticipants.successMessage}>
-                            Presença confirmada com sucesso!
-                        </p>
-                    ) : (
-                        <form className={EventParticipants.formContainer} onSubmit={handleSubmit}>
-                            <h1>Insira seu R.A. para confirmar sua presença</h1>
-                            <input 
-                                type="text" 
-                                placeholder='R.A.' 
-                                value={ra}
-                                onChange={(e) => setRa(e.target.value)}
-                            />
-                            <button 
-                                type="submit"
-                                className={`${EventParticipants.conteinerConfirmButton} ${urbanist.className}`} 
-                                disabled={!coords || !ra}
-                            >
-                                Confirmar Presença
-                            </button>
-                        </form>
+            <div className={EventParticipants.modalContent}>
+                {presenceConfirmed ? (
+                <p className={EventParticipants.successMessage}>
+                    Presença confirmada com sucesso!
+                </p>
+                ) : (
+                <form className={EventParticipants.formContainer} onSubmit={handleSubmit}>
+                    <h1>Insira seu R.A. para confirmar sua presença</h1>
+                    <input 
+                    type="text" 
+                    placeholder='R.A.' 
+                    value={ra}
+                    onChange={(e) => setRa(e.target.value)}
+                    />
+                    <button 
+                    type="submit"
+                    className={`${EventParticipants.conteinerConfirmButton} ${urbanist.className}`} 
+                    disabled={!coords || !ra}
+                    >
+                    Confirmar Presença
+                    </button>
+                    {error && (
+                    <p className={EventParticipants.errorMessage}>
+                        {error}
+                    </p>
                     )}
-                </div>
+                </form>
+                )}
+            </div>
             </Modal>
 
             {presenceConfirmed && (
-                <div className={EventParticipants.toastMessage}>
-                    Presença confirmada com sucesso!
-                </div>
+            <div className={EventParticipants.toastMessage}>
+                Presença confirmada com sucesso!
+            </div>
             )}
         </>
     );
